9e36ce482c124ea5cdc2b8920e0b557a94b10f85
Martosky <imoniaromartins@yahoo.com>
2025-02-20 10:18:05 +0100

    coded the logic that convers integer into it equivalent roman numeras

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const output = document.getElementById("output")
 const button = document.getElementById("convert-btn")
-const input = document.getElementById("input")
+const input = document.getElementById("number")
+
+button.addEventListener("click", displayResult)
 
 function displayResult(){
    let finalResult = ""
@@ -16,4 +18,23 @@ function displayResult(){
         output.innerText = finalResult
     }
     return finalResult
+}
+
+const dataArray = [
+    {value: 1000, Symbol: "M"},
+    {value: 900, Symbol: "DM"},
+    {value: 500, Symbol: "D"},
+    {value: 400, Symbol: "CD"},
+    {value: 100, Symbol: "C"},
+    {value: 90, Symbol: "LC"},
+    {value: 50, Symbol: "L"},
+    {value: 40, Symbol: "XL"},
+    {value: 10, Symbol: "X"},
+    {value: 9, Symbol: "IX"},
+    {value: 5, Symbol: "V"},
+    {value: 4, Symbol: "IV"},
+    {value: 1, Symbol: "I"}
+]
+function romanFigure(n){
+
 }
